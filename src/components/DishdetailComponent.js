fadda2ece22dfce7b6420e57a60bc0640f344f09
Jiachen Ning <kellyning25@gmail.com>
2020-04-10 13:27:24 -0400

    changed structure of dishdetailcomponent and allows for redux action of adding comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,23 +6,72 @@ import { Card, CardImg, CardText, CardBody, Button,
 import { Link } from 'react-router-dom';
 
 
-class DishDetail extends Component {
-  constructor(props) {
-        super(props);
-
-        // when click "comment button", display the form
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleComments = this.handleComments.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-
-        this.state = {
-            isModalOpen: false,
-            name: "",
-            touched: {
-                name: false
+function renderComments(comments, addComment, dishId) {
+      if (comments != null) 
+        return(
+          <div>
+            <h4>Comments</h4>
+            {comments.map((c) => {
+                  return (
+                    <div key={c.id} >
+                        <ul className = "list-unstyled">
+                          <li>{c.comment}</li>
+                          <li>--{c.author},&nbsp;
+                            {new Intl.DateTimeFormat
+                            ('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format
+                            (new Date(Date.parse(c.date)))}
+                          </li>
+                        </ul>
+                    </div>
+                  );
+              })
             }
-        };
+            <CommentForm dishId={dishId} addComment={addComment} />
+          </div>
+        );
+      else
+        return(
+          <div></div>
+        );
+}
+
+function renderDish(dish) {
+    if (dish != null)
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    else
+        return(
+            <div></div>
+        );
     }
+
+
+
+class CommentForm extends Component {
+
+  constructor(props) {
+      super(props);
+
+      // when click "comment button", display the form
+      this.toggleModal = this.toggleModal.bind(this);
+      this.handleComments = this.handleComments.bind(this);
+      this.handleInputChange = this.handleInputChange.bind(this);
+
+      this.state = {
+          isModalOpen: false,
+          name: "",
+          touched: {
+              name: false
+          }
+      };
+  }
 
   toggleModal() {
       this.setState({
@@ -30,11 +79,9 @@ class DishDetail extends Component {
       });
     }
 
-  handleComments(event) {
+  handleComments(values) {
       this.toggleModal();
-      alert("Rating: " + this.rating.value + " Your Name: " + this.name.value
-          + " Comment: " + this.comment.value);
-      event.preventDefault();
+      this.props.addComment(this.props.dishId, this.rating.value, this.name.value, this.comment.value);
     }
 
   handleBlur = (field) => (evt) => {
@@ -68,80 +115,14 @@ class DishDetail extends Component {
         return errors;
     }
 
-  renderDish(dish) {
-    if (dish != null)
-        return(
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-    else
-        return(
-            <div></div>
-        );
-    }
-
-    renderComments(comments) {
-      if (comments != null) 
-        return(
-          <div>
-            <h4>Comments</h4>
-            {comments}
-          </div>
-        );
-      else
-        return(
-          <div></div>
-        );
-    }
+  
 
     render() {
       const errors = this.validate(this.state.name);
 
-      console.log("!!!")
-      console.log(this.props)
-      const comments = this.props.comments.map((c) => {
-            return (
-              <div key={c.id} >
-                  <ul className = "list-unstyled">
-                    <li>{c.comment}</li>
-                    <li>--{c.author},&nbsp;
-                      {new Intl.DateTimeFormat
-                      ('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format
-                      (new Date(Date.parse(c.date)))}
-                    </li>
-                  </ul>
-              </div>
-            );
-        });
-
-        return (
-              <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{this.props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(comments)}
-                        <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
-                    </div>
-                </div>
-
+      return (
+            <div>
+              <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
               <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
@@ -184,7 +165,39 @@ class DishDetail extends Component {
               </div>
             );
 
-    }
+      }
+  }
+ 
+ const DishDetail = (props) => {
+    if (props.dish != null)
+        return (
+              <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {renderDish(props.dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {renderComments(props.comments, props.addComment, props.dish.id)}
+                    </div>
+                </div>
+              </div>
+            );
+    else
+        return(
+            <div>
+            </div>
+        );
 }
 
 export default DishDetail;
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,7 +1,15 @@
 import { COMMENTS } from '../shared/comments';
+import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
     switch (action.type) {
+    	// if the action type matches ADD_COMMENT
+        case ActionTypes.ADD_COMMENT:
+            var comment = action.payload;
+            comment.id = state.length;
+            comment.date = new Date().toISOString();
+            console.log("Comment: ", comment);
+            return state.concat(comment);
 
         default:
           return state;
